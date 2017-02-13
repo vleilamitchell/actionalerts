2566c0378bf3c7e757fa049ffb48e3bba4982977
Victoria Mitchell <vleilamitchell@gmail.com>
2017-02-13 17:05:51 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+app.get('/', (req, res) => {
+  res.send('ActionAlerts Online!')
+})
+
+app.listen(3000, () => console.log('Server running on port 3000'))
